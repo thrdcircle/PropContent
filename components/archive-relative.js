81df7fc3a81e928f7f329c756dd0d2e1dab61949
thrdcircle <thrdcircle@hotmail.com>
2022-05-19 17:03:33 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ components/archive-relative.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Link from 'next/link';
+import parse from 'html-react-parser';
+
+export default function ArchiveRelative({ blogs }) {
+  return (
+    <>
+      {blogs?.map((blog, idx) => (
+        <Link href={blog.url} key={idx}>
+          <a>
+            <div>
+              <h4 {...blog.$?.title}>{blog.title}</h4>
+              {typeof blog.body === 'string' && (
+                <div {...blog.$?.body}>{parse(blog.body.slice(0, 80))}</div>
+              )}
+            </div>
+          </a>
+        </Link>
+      ))}
+    </>
+  );
+}
