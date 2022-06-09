ddc4590ca88db41bbdaeb96e06071c709188b015
thrdcircle <thrdcircle@hotmail.com>
2022-06-09 13:01:47 -0400

    Update render-components.js

--- components/render-components.js
+++ components/render-components.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import Section from './section';
 import HeroBanner from './hero-banner';
+import BlogBanner from './blog-banner';
 import CardSection from './card-section';
 import TeamSection from './team-section';
 import BlogSection from './blog-section';
