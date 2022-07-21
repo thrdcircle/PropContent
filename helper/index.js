59569247fd5765917dab593f46246671d254abea
thrdcircle <thrdcircle@hotmail.com>
2022-07-21 14:58:11 -0400

    Update index.js

--- helper/index.js
+++ helper/index.js
@@ -1,79 +1,90 @@
-import Stack from '../contentstack-sdk/index';
-import { addEditableTags } from '@contentstack/utils';
-import getConfig from 'next/config';
+import Stack from "../contentstack-sdk";
+import { addEditableTags } from "@contentstack/utils";
+import getConfig from "next/config";
 
 const { publicRuntimeConfig } = getConfig();
 const envConfig = process.env.CONTENTSTACK_API_KEY
   ? process.env
   : publicRuntimeConfig;
 
-const liveEdit = envConfig.CONTENTSTACK_LIVE_EDIT_TAGS === 'true';
+const liveEdit = envConfig.CONTENTSTACK_LIVE_EDIT_TAGS === "true";
 
 export const getHeaderRes = async () => {
   const response = await Stack.getEntry({
-    contentTypeUid: 'header',
-    referenceFieldPath: ['navigation_menu.page_reference'],
-    jsonRtePath: ['notification_bar.announcement_text'],
+    contentTypeUid: "header",
+    referenceFieldPath: ["navigation_menu.page_reference"],
+    jsonRtePath: ["notification_bar.announcement_text"],
   });
 
-  liveEdit && addEditableTags(response[0][0], 'header', true);
+  liveEdit && addEditableTags(response[0][0], "header", true);
   return response[0][0];
 };
 
 export const getFooterRes = async () => {
   const response = await Stack.getEntry({
-    contentTypeUid: 'footer',
+    contentTypeUid: "footer",
     referenceFieldPath: undefined,
-    jsonRtePath: ['copyright'],
+    jsonRtePath: ["copyright"],
   });
-  liveEdit && addEditableTags(response[0][0], 'footer', true);
+  liveEdit && addEditableTags(response[0][0], "footer", true);
   return response[0][0];
 };
 
 export const getAllEntries = async () => {
   const response = await Stack.getEntry({
-    contentTypeUid: 'page',
+    contentTypeUid: "page",
     referenceFieldPath: undefined,
     jsonRtePath: undefined,
   });
   liveEdit &&
-    response[0].forEach((entry) => addEditableTags(entry, 'page', true));
+    response[0].forEach((entry) => addEditableTags(entry, "page", true));
   return response[0];
 };
 
 export const getPageRes = async (entryUrl) => {
   const response = await Stack.getEntryByUrl({
-    contentTypeUid: 'page',
+    contentTypeUid: "page",
     entryUrl,
-    referenceFieldPath: ['page_components.from_blog.featured_blogs'],
+    referenceFieldPath: ["page_components.from_blog.featured_blogs"],
     jsonRtePath: [
-      'page_components.from_blog.featured_blogs.body',
-      'page_components.section_with_buckets.buckets.description',
-      'page_components.section_with_html_code.description',
+      "page_components.from_blog.featured_blogs.body",
+      "page_components.section_with_buckets.buckets.description",
+      "page_components.section_with_html_code.description",
     ],
   });
-  liveEdit && addEditableTags(response[0], 'page', true);
+  liveEdit && addEditableTags(response[0], "page", true);
   return response[0];
 };
 
 export const getBlogListRes = async () => {
   const response = await Stack.getEntry({
-    contentTypeUid: 'blog_post',
-    referenceFieldPath: ['author', 'related_post'],
-    jsonRtePath: ['body'],
+    contentTypeUid: "blog_post",
+    referenceFieldPath: ["author", "related_post"],
+    jsonRtePath: ["body"],
   });
   liveEdit &&
-    response[0].forEach((entry) => addEditableTags(entry, 'blog_post', true));
+    response[0].forEach((entry) => addEditableTags(entry, "blog_post", true));
   return response[0];
 };
 
 export const getBlogPostRes = async (entryUrl) => {
   const response = await Stack.getEntryByUrl({
-    contentTypeUid: 'blog_post',
+    contentTypeUid: "blog_post",
     entryUrl,
-    referenceFieldPath: ['author', 'related_post'],
-    jsonRtePath: ['body', 'related_post.body'],
+    referenceFieldPath: ["author", "related_post"],
+    jsonRtePath: ["body", "related_post.body"],
   });
-  liveEdit && addEditableTags(response[0], 'blog_post', true);
+  liveEdit && addEditableTags(response[0], "blog_post", true);
+  return response[0];
+};
+
+export const getArticleRes = async (entryUrl) => {
+  const response = await Stack.getEntryByUrl({
+    contentTypeUid: "article",
+    entryUrl,
+    referenceFieldPath: ["author", "related_post"],
+    jsonRtePath: ["body", "related_post.body"],
+  });
+  liveEdit && addEditableTags(response[0], "article", true);
   return response[0];
 };
